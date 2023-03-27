07d7f421d7b8ea2c1cf307d794b375e929aa6c7d
mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2023-03-27 21:03:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,99 @@
 from pyrogram import Client, filters
+import time
+import math
+import ffmpeg
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+
+def progress_bar(current, total, text, message, start, userbot):
+
+    now = time.time()
+    diff = now-start
+    if round(diff % 10) == 0 or current == total:
+        percentage = current*100/total
+        speed = current/diff
+        elapsed_time = round(diff)*1000
+        eta = round((total-current)/speed)*1000
+        ett = eta + elapsed_time
+
+        elapsed_time = TimeFormatter(elapsed_time)
+        ett = TimeFormatter(ett)
+
+        progress = "[{0}{1}] \n**İlerleme**: {2}%\n".format(
+            ''.join(["●" for i in range(math.floor(percentage / 10))]),
+            ''.join(["○" for i in range(10 - math.floor(percentage / 10))]),
+            round(percentage, 2))
+
+        tmp = progress + "**Indirilen**: {0}/{1}\n**Hız**: `{2}`/s\n**Tahmini Süre**: `{3}`\n".format(
+            humanbytes(current),
+            humanbytes(total),
+            humanbytes(speed),
+            # elapsed_time if elapsed_time != '' else "0 s",
+            ett if ett != '' else "0 s"
+        )
+
+        try :
+            userbot.message.edit(
+                text = '{}'.format(tmp)
+            )
+        except:
+            pass
+
+def humanbytes(size):
+    # https://stackoverflow.com/a/49361727/4723940
+    # 2**10 = 1024
+    if not size:
+        return ""
+    power = 2**10
+    n = 0
+    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
+
+def TimeFormatter(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + "d, ") if days else "") + \
+        ((str(hours) + "h, ") if hours else "") + \
+        ((str(minutes) + "m, ") if minutes else "") + \
+        ((str(seconds) + "s, ") if seconds else "") + \
+        ((str(milliseconds) + "ms, ") if milliseconds else "")
+    return tmp[:-2]
+
+
+def get_thumbnail(in_filename, path, ttl):
+    out_filename = os.path.join(path, str(time.time()) + ".jpg")
+    open(out_filename, 'a').close()
+    try:
+        (
+            ffmpeg
+            .input(in_filename, ss=ttl)
+            .output(out_filename, vframes=1)
+            .overwrite_output()
+            .run(capture_stdout=True, capture_stderr=True)
+        )
+        return out_filename
+    except ffmpeg.Error as e:
+      return None
+
+def get_duration(filepath):
+    metadata = extractMetadata(createParser(filepath))
+    if metadata.has("duration"):
+      return metadata.get('duration').seconds
+    else:
+      return 0
+
+def get_width_height(filepath):
+    metadata = extractMetadata(createParser(filepath))
+    if metadata.has("width") and metadata.has("height"):
+      return metadata.get("width"), metadata.get("height")
+    else:
+      return 1280, 720
 
 
 try:
@@ -12,8 +107,24 @@ try:
     me = userbot.get_me()
     userbot.send_message(OWNER_ID, f"Userbot Bașlatıldı..\n\n**Premium Durumu**: {me.is_premium}\n**Ad**: {me.first_name}\n**id**: {me.id}")
     video = 
+    duration = get_duration(video)
+    DOWNLOAD_DIR = "indirilenler"
+    thumb = get_thumbnail(video, './' + DOWNLOAD_DIR, duration / 4)
+    width, height = get_width_height(video)
+    start_time=time.time()
+    m = userbot.send_message(OWNER_ID, "'Video Yükleniyor..'")
     userbot.send_video(
         chat_id=OWNER_ID,
-        video=video)
+        video=video,
+        thumb=thumb,
+        duration=duration,
+        width=width, 
+        height=height,
+        progress = progress_bar, 
+        progress_args = (
+            'Yükleniyor!',
+            m,
+            start_time,
+            userbot))
 except Exception as e:
     print(e)
